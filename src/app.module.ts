4a0375db4886e4629c8ffb56fb28c145167cffc9
Fadi Serapian <fadi.serapian@gmail.com>
2022-02-22 19:44:06 -0500

    Init new article module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArticleModule } from './article/article.module';
 import ormconfig from './ormconfig';
 import { TagModule } from './tag/tag.module';
 import { AuthMiddleware } from './user/middlewares/auth.middleware';
@@ -13,6 +14,7 @@ import { UserModule } from './user/user.module';
     TypeOrmModule.forRoot(ormconfig),
     TagModule,
     UserModule,
+    ArticleModule,
   ],
   controllers: [AppController],
   providers: [AppService],
