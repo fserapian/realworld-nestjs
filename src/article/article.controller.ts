e5b8a660f55ae464dd98d10d252eea4b33574728
Fadi Serapian <fadi.serapian@gmail.com>
2022-02-23 18:17:44 -0500

    Add create article dto and logic to article controller and service

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { CurrentUser } from 'src/user/decorators/current-user.decorator';
+import { AuthGuard } from 'src/user/guards/auth.guard';
+import { User } from 'src/user/user.entity';
 import { ArticleService } from './article.service';
+import { CreateArticleDto } from './dto/create-article.dto';
 
 @Controller('articles')
 export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
 
   @Post()
-  createArticle() {
-    return this.articleService.createArticle();
+  @UseGuards(AuthGuard)
+  async createArticle(
+    @CurrentUser() currentUser: User,
+    @Body('article') createArticleDto: CreateArticleDto,
+  ): Promise<any> {
+    return this.articleService.createArticle(currentUser, createArticleDto);
   }
 }
--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/user/user.entity';
+import { Repository } from 'typeorm';
+import { Article } from './article.entity';
+import { CreateArticleDto } from './dto/create-article.dto';
 
 @Injectable()
 export class ArticleService {
-  createArticle() {
-    return 'Create article from service';
+  constructor(
+    @InjectRepository(Article)
+    private readonly articleRepository: Repository<Article>,
+  ) {}
+
+  async createArticle(
+    currentUser: User,
+    createArticleDto: CreateArticleDto,
+  ): Promise<Article> {
+    const article = new Article();
+    Object.assign(article, createArticleDto);
+
+    if (!article.tagList) {
+      article.tagList = [];
+    }
+
+    article.author = currentUser;
+
+    article.slug = 'foo';
+
+    return await this.articleRepository.save(article);
   }
 }
