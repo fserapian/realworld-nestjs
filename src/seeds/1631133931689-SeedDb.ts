d238e0e9b85d761a39ba49d872f2ff8d75a96926
Fadi Serapian <fadi.serapian@gmail.com>
2022-03-21 18:35:11 -0400

    Add seeding functionality and default seeder for tags, users and articles

---
+++ src/seeds/1631133931689-SeedDb.ts
@@ -0,0 +1,16 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class SeedDb1631133931689 implements MigrationInterface {
+    name = 'SeedDb1631133931689'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`INSERT INTO \`mediumclone\`.\`tags\` (\`name\`) VALUES ('NestJS'), ('Coffee'), ('Angular'), ('React'), ('Vue'), ('Javascript'), ('Typescript'), ('NodeJS'), ('PHP'), ('Laravel'), ('Java')`);
+
+        // Password is password
+        await queryRunner.query(`INSERT INTO \`mediumclone\`.\`users\` (\`username\`, \`email\`, \`password\`) VALUES ('fadi', 'fadi@gmail.com', '$2b$10$czmuRSgK8ZaOQ5VW4drX3u47/dM2GORDM4ZQ/Ga3QcHrlCyFCe/62')`);
+
+        await queryRunner.query(`INSERT INTO \`mediumclone\`.\`articles\` (\`slug\`, \`title\`, \`description\`, \`body\`, \`tagList\`, \`authorId\`) VALUES ('first-article', 'First Article', 'First article description', 'First article body', 'Angular,Typescript', 1), ('second-article', 'Second Article', 'Second article description', 'Second article body', 'React,Javascript', 1)`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {}
+}
